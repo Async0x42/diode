a0d347a6c195763044551b1a20b88796463773ad
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-12 15:25:48 -0400

    update eslint

--- packages/common/.eslintrc.js
+++ packages/common/.eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ['@antfu', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 };
--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['@antfu', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
   rules: {
     'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 };
