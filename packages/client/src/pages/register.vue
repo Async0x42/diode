fcb520dcf1bbeea8802cde027b3fa8ac8213e29a
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-13 15:45:07 -0400

    cleanup register page

--- packages/client/src/pages/register.vue
+++ packages/client/src/pages/register.vue
@@ -1,3 +1,52 @@
+<script setup lang="ts">
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import { computed, onMounted, ref } from 'vue';
+import * as yup from 'yup';
+import { useRouter } from 'vue-router';
+import { state as authState, register } from '~/logic';
+const router = useRouter();
+
+const schema = yup.object().shape({
+  email: yup.string().required('Email is required!').email('Email is invalid!').max(50, 'Must be maximum 50 characters!'),
+  password: yup.string().required('Password is required!').min(6, 'Must be at least 6 characters!').max(40, 'Must be maximum 40 characters!'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null])
+    .required('Password confirm is required'),
+});
+
+const successful = ref(false);
+const loading = ref(false);
+const message = ref('');
+
+const loggedIn = computed(() => authState.value.status.loggedIn);
+
+onMounted(() => {
+  if (loggedIn.value) {
+    router.push({ name: 'dashboard' });
+  }
+});
+
+const handleRegister = (user) => {
+  message.value = '';
+  successful.value = false;
+  loading.value = true;
+
+  register(user).then(
+    (data) => {
+      message.value = data.message;
+      successful.value = true;
+      loading.value = false;
+    },
+    (error) => {
+      message.value = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+      successful.value = false;
+      loading.value = false;
+    }
+  );
+};
+</script>
+
 <template>
   <div class="flex flex-col min-h-screen bg-gray-50 py-12 justify-center sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:max-w-md sm:w-full">
@@ -67,69 +116,6 @@
     </div>
   </div>
 </template>
-
-<script>
-import { Form, Field, ErrorMessage } from 'vee-validate';
-import * as yup from 'yup';
-import { state as authState, register } from '~/logic';
-
-export default {
-  name: 'Register',
-  components: {
-    Form,
-    Field,
-    ErrorMessage,
-  },
-  data() {
-    const schema = yup.object().shape({
-      email: yup.string().required('Email is required!').email('Email is invalid!').max(50, 'Must be maximum 50 characters!'),
-      password: yup.string().required('Password is required!').min(6, 'Must be at least 6 characters!').max(40, 'Must be maximum 40 characters!'),
-      confirmPassword: yup
-        .string()
-        .oneOf([yup.ref('password'), null])
-        .required('Password confirm is required'),
-    });
-
-    return {
-      successful: false,
-      loading: false,
-      message: '',
-      schema,
-    };
-  },
-  computed: {
-    loggedIn() {
-      return authState.value.status.loggedIn;
-    },
-  },
-  mounted() {
-    if (this.loggedIn) {
-      this.$router.push('/profile');
-    }
-  },
-  methods: {
-    handleRegister(user) {
-      this.message = '';
-      this.successful = false;
-      this.loading = true;
-      console.log(user);
-
-      register(user).then(
-        (data) => {
-          this.message = data.message;
-          this.successful = true;
-          this.loading = false;
-        },
-        (error) => {
-          this.message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
-          this.successful = false;
-          this.loading = false;
-        }
-      );
-    },
-  },
-};
-</script>
 
 <route lang="yaml">
 name: register
