824414d79689af5e6cbfa97a692fd553c2cd2a16
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-16 21:47:55 -0400

    search for calendar item list

--- packages/client/src/components/CalendarItemList.vue
+++ packages/client/src/components/CalendarItemList.vue
@@ -2,14 +2,17 @@
 import { defineProps } from 'vue';
 import type { CalendarItemAttributes } from '@daiod/common';
 import type { PropType } from 'vue';
+import { useRouteSearchWithData } from '~/logic';
 
 const props = defineProps({
   items: { type: Array as PropType<CalendarItemAttributes[]>, required: true },
 });
+
+const { results } = useRouteSearchWithData(props.items, ['title', 'category', 'description', 'start', 'end']);
 </script>
 
 <template>
-  <div v-for="item in props.items" :key="item.id">
+  <div v-for="item in results" :key="item.id">
     {{ item.title }}
   </div>
 </template>
