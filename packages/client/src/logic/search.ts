0bce1b0275aebc91eff62b42531d0a2b3ef68664
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-16 20:26:21 -0400

    Update search.ts

--- packages/client/src/logic/search.ts
+++ packages/client/src/logic/search.ts
@@ -20,7 +20,9 @@ export const useRouteSearch = () => {
   return useSearch(route.path);
 };
 
+// see this for typing issues: https://github.com/vuejs/vue-next/issues/2136
 export const useRouteSearchWithData = <T>(data: T[], keys: string[]) => {
+  const searchData = reactive(data);
   const search = useRouteSearch();
   const options: Fuse.IFuseOptions<T> = {
     threshold: 0,
@@ -32,13 +34,13 @@ export const useRouteSearchWithData = <T>(data: T[], keys: string[]) => {
 
   watchEffect(() => {
     if (search.value.length === 0) {
-      results.value = reactive(data);
+      results.value = searchData;
     } else {
       const searchResults = fuse.search(search.value);
       const searchIds = searchResults.map((r) => r.refIndex);
-      results.value = reactive(data.filter((contact, index) => searchIds.includes(index)));
+      results.value = searchData.filter((contact, index) => searchIds.includes(index));
     }
   });
 
-  return { search, results: ref(results) };
+  return { search, results };
 };
