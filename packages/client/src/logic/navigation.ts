4187ce43deae34f2dd0b48597dd47a1a6fac458d
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-17 15:10:40 -0400

    remove index and replace w/ dashboard

--- packages/client/src/logic/navigation.ts
+++ packages/client/src/logic/navigation.ts
@@ -2,7 +2,7 @@ import { DocumentTextIcon, ClockIcon, HomeIcon, PhoneIcon, CalendarIcon } from '
 import { ref } from 'vue';
 
 export const navigation = ref([
-  { name: 'Dashboard', to: { name: 'dashboard' }, icon: HomeIcon },
+  { name: 'Dashboard', to: { name: 'home' }, icon: HomeIcon },
   { name: 'Calendar', to: { name: 'calendar' }, icon: CalendarIcon },
   { name: 'Contacts', to: { name: 'contacts' }, icon: PhoneIcon },
   { name: 'BRDs', to: { name: 'brds' }, icon: DocumentTextIcon },
--- packages/client/src/pages/index.vue
+++ packages/client/src/pages/index.vue
@@ -1,57 +1,341 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useI18n } from 'vue-i18n';
-
-const name = ref('');
-
-const router = useRouter();
-const go = () => {
-  // if (name.value) router.push(`/hi/${encodeURIComponent(name.value)}`);
-  if (name.value) router.push(`/dashboard`);
-};
-
-const { t } = useI18n();
+import { activity } from '~/logic';
 </script>
 
 <template>
-  <div>
-    <p class="text-4xl">
-      <carbon-campsite class="inline-block" />
-    </p>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank"> Vitesse </a>
-    </p>
-    <p>
-      <em class="text-sm opacity-75">{{ t('intro.desc') }}</em>
-    </p>
+  <div class="py-8 xl:py-10">
+    <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 xl:max-w-5xl xl:grid xl:grid-cols-3">
+      <div class="xl:border-r xl:border-gray-200 xl:pr-8 xl:col-span-2">
+        <div>
+          <div>
+            <div class="md:flex md:space-x-4 md:items-center md:justify-between xl:border-b xl:pb-6">
+              <div>
+                <h1 class="font-bold text-2xl text-gray-900">ARIA attribute misspelled</h1>
+                <p class="mt-2 text-sm text-gray-500">
+                  #400 opened by
+                  {{ ' ' }}
+                  <a href="#" class="font-medium text-gray-900">Hilary Mahy</a>
+                  {{ ' ' }}
+                  in
+                  {{ ' ' }}
+                  <a href="#" class="font-medium text-gray-900">Customer Portal</a>
+                </p>
+              </div>
+              <div class="flex space-x-3 mt-4 md:mt-0">
+                <button
+                  type="button"
+                  class="bg-white border rounded-md font-medium border-gray-300 shadow-sm text-sm py-2 px-4 text-gray-700 inline-flex justify-center hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                >
+                  <heroicons-solid-pencil class="h-5 mr-2 -ml-1 text-gray-400 w-5" aria-hidden="true" />
+                  <span>Edit</span>
+                </button>
+                <button
+                  type="button"
+                  class="bg-white border rounded-md font-medium border-gray-300 shadow-sm text-sm py-2 px-4 text-gray-700 inline-flex justify-center hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                >
+                  <heroicons-solid-bell class="h-5 mr-2 -ml-1 text-gray-400 w-5" aria-hidden="true" />
+                  <span>Subscribe</span>
+                </button>
+              </div>
+            </div>
+            <aside class="mt-8 xl:hidden">
+              <h2 class="sr-only">Details</h2>
+              <div class="space-y-5">
+                <div class="flex space-x-2 items-center">
+                  <heroicons-solid-lock-open class="h-5 text-green-500 w-5" aria-hidden="true" />
+                  <span class="font-medium text-sm text-green-700">Open Issue</span>
+                </div>
+                <div class="flex space-x-2 items-center">
+                  <heroicons-solid-chat-alt class="h-5 text-gray-400 w-5" aria-hidden="true" />
+                  <span class="font-medium text-sm text-gray-900">4 comments</span>
+                </div>
+                <div class="flex space-x-2 items-center">
+                  <heroicons-solid-calendar class="h-5 text-gray-400 w-5" aria-hidden="true" />
+                  <span class="font-medium text-sm text-gray-900">Created on <time datetime="2020-12-02">Dec 2, 2020</time></span>
+                </div>
+              </div>
+              <div class="border-t border-b space-y-8 border-gray-200 mt-6 py-6">
+                <div>
+                  <h2 class="font-medium text-sm text-gray-500">Assignees</h2>
+                  <ul class="space-y-3 mt-3">
+                    <li class="flex justify-start">
+                      <a href="#" class="flex space-x-3 items-center">
+                        <div class="flex-shrink-0">
+                          <img
+                            class="rounded-full h-5 w-5"
+                            src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
+                            alt=""
+                          />
+                        </div>
+                        <div class="font-medium text-sm text-gray-900">Eduardo Benz</div>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <h2 class="font-medium text-sm text-gray-500">Tags</h2>
+                  <ul class="mt-2 leading-8">
+                    <li class="inline">
+                      <a href="#" class="border rounded-full border-gray-300 py-0.5 px-3 relative inline-flex items-center">
+                        <div class="flex flex-shrink-0 absolute items-center justify-center">
+                          <span class="rounded-full bg-rose-500 h-1.5 w-1.5" aria-hidden="true" />
+                        </div>
+                        <div class="font-medium text-sm ml-3.5 text-gray-900">Bug</div>
+                      </a>
+                      {{ ' ' }}
+                    </li>
+                    <li class="inline">
+                      <a href="#" class="border rounded-full border-gray-300 py-0.5 px-3 relative inline-flex items-center">
+                        <div class="flex flex-shrink-0 absolute items-center justify-center">
+                          <span class="rounded-full bg-indigo-500 h-1.5 w-1.5" aria-hidden="true" />
+                        </div>
+                        <div class="font-medium text-sm ml-3.5 text-gray-900">Accessibility</div>
+                      </a>
+                      {{ ' ' }}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </aside>
+            <div class="py-3 xl:pt-6 xl:pb-0">
+              <h2 class="sr-only">Description</h2>
+              <div class="max-w-none prose">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, hic? Commodi cumque similique id tempora molestiae deserunt at
+                  suscipit, dolor voluptatem, numquam, harum consequatur laboriosam voluptas tempore aut voluptatum alias?
+                </p>
+                <ul>
+                  <li>
+                    Tempor ultrices proin nunc fames nunc ut auctor vitae sed. Eget massa parturient vulputate fermentum id facilisis nam pharetra.
+                    Aliquet leo tellus.
+                  </li>
+                  <li>Turpis ac nunc adipiscing adipiscing metus tincidunt senectus tellus.</li>
+                  <li>Semper interdum porta sit tincidunt. Dui suspendisse scelerisque amet metus eget sed. Ut tellus in sed dignissim.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section aria-labelledby="activity-title" class="mt-8 xl:mt-10">
+          <div>
+            <div class="divide-y divide-gray-200">
+              <div class="pb-4">
+                <h2 id="activity-title" class="font-medium text-lg text-gray-900">Activity</h2>
+              </div>
+              <div class="pt-6">
+                <!-- Activity feed-->
+                <div class="flow-root">
+                  <ul class="-mb-8">
+                    <li v-for="(item, itemIdx) in activity" :key="item.id">
+                      <div class="pb-8 relative">
+                        <span
+                          v-if="itemIdx !== activity.length - 1"
+                          class="h-full -ml-px bg-gray-200 top-5 left-5 w-0.5 absolute"
+                          aria-hidden="true"
+                        />
+                        <div class="flex space-x-3 relative items-start">
+                          <template v-if="item.type === 'comment'">
+                            <div class="relative">
+                              <img
+                                class="rounded-full flex bg-gray-400 h-10 ring-white ring-8 w-10 items-center justify-center"
+                                :src="item.imageUrl"
+                                alt=""
+                              />
 
-    <div class="py-4" />
+                              <span class="bg-white rounded-tl py-px px-0.5 -right-1 -bottom-0.5 absolute">
+                                <heroicons-solid-chat-alt class="h-5 text-gray-400 w-5" aria-hidden="true" />
+                              </span>
+                            </div>
+                            <div class="flex-1 min-w-0">
+                              <div>
+                                <div class="text-sm">
+                                  <a :href="item.person.href" class="font-medium text-gray-900">{{ item.person.name }}</a>
+                                </div>
+                                <p class="mt-0.5 text-sm text-gray-500">Commented {{ item.date }}</p>
+                              </div>
+                              <div class="mt-2 text-sm text-gray-700">
+                                <p>
+                                  {{ item.comment }}
+                                </p>
+                              </div>
+                            </div>
+                          </template>
+                          <template v-else-if="item.type === 'assignment'" condition="item.type === 'assignment'">
+                            <div>
+                              <div class="px-1 relative">
+                                <div class="rounded-full flex bg-gray-100 h-8 ring-white ring-8 w-8 items-center justify-center">
+                                  <heroicons-solid-user-circle class="h-5 text-gray-500 w-5" aria-hidden="true" />
+                                </div>
+                              </div>
+                            </div>
+                            <div class="flex-1 min-w-0 py-1.5">
+                              <div class="text-sm text-gray-500">
+                                <a :href="item.person.href" class="font-medium text-gray-900">{{ item.person.name }}</a>
+                                {{ ' ' }}
+                                <!-- not really needed if item.type is assignment, better typings can remove this -->
+                                <template v-if="item.assigned">
+                                  assigned
+                                  {{ ' ' }}
+                                  <a :href="item.assigned.href" class="font-medium text-gray-900">{{ item.assigned.name }}</a>
+                                  {{ ' ' }}
+                                </template>
+                                <span class="whitespace-nowrap">{{ item.date }}</span>
+                              </div>
+                            </div>
+                          </template>
+                          <template v-else>
+                            <div>
+                              <div class="px-1 relative">
+                                <div class="rounded-full flex bg-gray-100 h-8 ring-white ring-8 w-8 items-center justify-center">
+                                  <heroicons-solid-tag class="h-5 text-gray-500 w-5" aria-hidden="true" />
+                                </div>
+                              </div>
+                            </div>
+                            <div class="flex-1 min-w-0 py-0">
+                              <div class="text-sm text-gray-500 leading-8">
+                                <span class="mr-0.5">
+                                  <a :href="item.person.href" class="font-medium text-gray-900">{{ item.person.name }}</a>
+                                  {{ ' ' }}
+                                  added tags
+                                </span>
+                                {{ ' ' }}
+                                <span class="mr-0.5">
+                                  <template v-for="tag in item.tags" :key="tag.name">
+                                    <a
+                                      :href="tag.href"
+                                      class="border rounded-full border-gray-300 text-sm py-0.5 px-3 relative inline-flex items-center"
+                                    >
+                                      <span class="flex flex-shrink-0 absolute items-center justify-center">
+                                        <span :class="[tag.color, 'h-1.5 w-1.5 rounded-full']" aria-hidden="true" />
+                                      </span>
+                                      <span class="font-medium ml-3.5 text-gray-900">{{ tag.name }}</span>
+                                    </a>
+                                    {{ ' ' }}
+                                  </template>
+                                </span>
+                                <span class="whitespace-nowrap">{{ item.date }}</span>
+                              </div>
+                            </div>
+                          </template>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div class="mt-6">
+                  <div class="flex space-x-3">
+                    <div class="flex-shrink-0">
+                      <div class="relative">
+                        <img
+                          class="rounded-full flex bg-gray-400 h-10 ring-white ring-8 w-10 items-center justify-center"
+                          src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
+                          alt=""
+                        />
 
-    <input
-      id="input"
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      class="bg-transparent border rounded outline-none border-gray-200 text-sm text-center py-2 px-4 dark:border-gray-700 active:outline-none"
-      style="width: 250px"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button class="m-3 text-sm btn" :disabled="!name" @click="go">
-        {{ t('button.go') }}
-      </button>
+                        <span class="bg-white rounded-tl py-px px-0.5 -right-1 -bottom-0.5 absolute">
+                          <heroicons-solid-chat-alt class="h-5 text-gray-400 w-5" aria-hidden="true" />
+                        </span>
+                      </div>
+                    </div>
+                    <div class="flex-1 min-w-0">
+                      <form action="#">
+                        <div>
+                          <label for="comment" class="sr-only">Comment</label>
+                          <textarea
+                            id="comment"
+                            name="comment"
+                            :rows="3"
+                            class="rounded-md border-gray-300 shadow-sm w-full block sm:text-sm focus:border-gray-900 focus:ring-gray-900"
+                            placeholder="Leave a comment"
+                          />
+                        </div>
+                        <div class="flex space-x-4 mt-6 items-center justify-end">
+                          <button
+                            type="button"
+                            class="bg-white border rounded-md font-medium border-gray-300 shadow-sm text-sm py-2 px-4 text-gray-700 inline-flex justify-center hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                          >
+                            <heroicons-solid-check-circle class="h-5 mr-2 -ml-1 text-green-500 w-5" aria-hidden="true" />
+                            <span>Close issue</span>
+                          </button>
+                          <button
+                            type="submit"
+                            class="border border-transparent rounded-md font-medium bg-gray-900 shadow-sm text-sm text-white py-2 px-4 inline-flex items-center justify-center hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                          >
+                            Comment
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <aside class="hidden xl:pl-8 xl:block">
+        <h2 class="sr-only">Details</h2>
+        <div class="space-y-5">
+          <div class="flex space-x-2 items-center">
+            <heroicons-solid-lock-open class="h-5 text-green-500 w-5" aria-hidden="true" />
+            <span class="font-medium text-sm text-green-700">Open Issue</span>
+          </div>
+          <div class="flex space-x-2 items-center">
+            <heroicons-solid-chat-alt class="h-5 text-gray-400 w-5" aria-hidden="true" />
+            <span class="font-medium text-sm text-gray-900">4 comments</span>
+          </div>
+          <div class="flex space-x-2 items-center">
+            <heroicons-solid-calendar class="h-5 text-gray-400 w-5" aria-hidden="true" />
+            <span class="font-medium text-sm text-gray-900">Created on <time datetime="2020-12-02">Dec 2, 2020</time></span>
+          </div>
+        </div>
+        <div class="border-t space-y-8 border-gray-200 mt-6 py-6">
+          <div>
+            <h2 class="font-medium text-sm text-gray-500">Assignees</h2>
+            <ul class="space-y-3 mt-3">
+              <li class="flex justify-start">
+                <a href="#" class="flex space-x-3 items-center">
+                  <div class="flex-shrink-0">
+                    <img
+                      class="rounded-full h-5 w-5"
+                      src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div class="font-medium text-sm text-gray-900">Eduardo Benz</div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 class="font-medium text-sm text-gray-500">Tags</h2>
+            <ul class="mt-2 leading-8">
+              <li class="inline">
+                <a href="#" class="border rounded-full border-gray-300 py-0.5 px-3 relative inline-flex items-center">
+                  <div class="flex flex-shrink-0 absolute items-center justify-center">
+                    <span class="rounded-full bg-rose-500 h-1.5 w-1.5" aria-hidden="true" />
+                  </div>
+                  <div class="font-medium text-sm ml-3.5 text-gray-900">Bug</div>
+                </a>
+                {{ ' ' }}
+              </li>
+              <li class="inline">
+                <a href="#" class="border rounded-full border-gray-300 py-0.5 px-3 relative inline-flex items-center">
+                  <div class="flex flex-shrink-0 absolute items-center justify-center">
+                    <span class="rounded-full bg-indigo-500 h-1.5 w-1.5" aria-hidden="true" />
+                  </div>
+                  <div class="font-medium text-sm ml-3.5 text-gray-900">Accessibility</div>
+                </a>
+                {{ ' ' }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </aside>
     </div>
   </div>
 </template>
 
 <route lang="yaml">
-name: landing
-
-meta:
-  layout: home
+name: home
 </route>
