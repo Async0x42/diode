d663f4826dc35ea264f1727d3296e34c51a6d148
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-18 11:23:46 -0400

    work work

--- packages/server/src/mikro-orm.config.ts
+++ packages/server/src/mikro-orm.config.ts
@@ -5,7 +5,8 @@ import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 const config: Configuration<IDatabaseDriver<Connection>> | Options<IDatabaseDriver<Connection>> = {
   metadataProvider: TsMorphMetadataProvider,
   highlighter: new SqlHighlighter(),
-  entitiesTs: ['./src/database/entities/**/*.ts'], // TS entities (source), relative to `baseDir`
+  entities: ['./dist/entities/**/*.js'], // TS entities (source), relative to `baseDir`
+  entitiesTs: ['./src/entities/**/*.ts'], // TS entities (source), relative to `baseDir`
   dbName: 'daiode',
   type: 'sqlite', // one of `mongo` | `mysql` | `mariadb` | `postgresql` | `sqlite`
   clientUrl: ':memory:',
