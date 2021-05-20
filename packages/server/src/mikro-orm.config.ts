9e2c192b70d0f38a693daf98ace810b70d472564
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-20 18:55:15 -0400

    entities -> .entity.ts

--- packages/server/src/mikro-orm.config.ts
+++ packages/server/src/mikro-orm.config.ts
@@ -5,8 +5,8 @@ import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 const config: Configuration<IDatabaseDriver<Connection>> | Options<IDatabaseDriver<Connection>> = {
   metadataProvider: TsMorphMetadataProvider,
   highlighter: new SqlHighlighter(),
-  entities: ['./**/entities/**/*.js'], // TS entities (source), relative to `baseDir`
-  entitiesTs: ['./**/entities/**/*.ts'], // TS entities (source), relative to `baseDir`
+  entities: ['./**/entities/**/*.entity.js'], // TS entities (source), relative to `baseDir`
+  entitiesTs: ['./**/entities/**/*.entity.ts'], // TS entities (source), relative to `baseDir`
   dbName: process.env.DB_NAME || 'diode.db',
   type: (process.env.DB_TYPE as keyof typeof Configuration.PLATFORMS) || 'sqlite',
   forceUtcTimezone: true,
