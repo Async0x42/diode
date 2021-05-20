becdc0bec2b118e7506e678bd9fbd1df0adddb44
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-19 23:44:48 -0400

    Update getItems.ts

--- packages/client/src/api/getItems.ts
+++ packages/client/src/api/getItems.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const getItems = async (calendarid: number) =>
+export const getItems = async (calendarId: number) =>
   await axios({
     method: 'get',
     url: `/api/calendars/${calendarId}/items`,
