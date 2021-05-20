9283981f969a06adb478aca0e357b1b5f3172132
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-20 16:20:44 -0400

    Update vite.config.ts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -111,7 +111,7 @@ export default defineConfig({
   },
 
   optimizeDeps: {
-    include: ['vue', 'vue-router', '@vueuse/core', '@vueuse/integrations'],
+    include: ['vue', 'vue-router', '@vueuse/core', '@vueuse/integrations', 'vue-hooks-form'],
     exclude: ['vue-demi'],
   },
 });
