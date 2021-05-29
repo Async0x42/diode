221b9c68df99abd6aa5746e6c4b4867be89d3b8d
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-28 20:47:04 -0400

    use mariadb driver instead of mysql

--- packages/server/src/mikro-orm.config.ts
+++ packages/server/src/mikro-orm.config.ts
@@ -1,15 +1,15 @@
 import { Configuration, Connection, IDatabaseDriver, Options } from '@mikro-orm/core';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
-import { MySqlDriver } from '@mikro-orm/mysql';
+import { MariaDbDriver } from '@mikro-orm/mariadb';
 
 const config: Configuration<IDatabaseDriver<Connection>> | Options<IDatabaseDriver<Connection>> = {
   metadataProvider: TsMorphMetadataProvider,
   highlighter: new SqlHighlighter(),
   entities: ['./**/entities/**/*.entity.js'], // TS entities (source), relative to `baseDir`
   entitiesTs: ['./**/entities/**/*.entity.ts'], // TS entities (source), relative to `baseDir`
-  driver: MySqlDriver,
-  type: 'mysql',
+  driver: MariaDbDriver,
+  type: 'mariadb',
 };
 
 export default config;
