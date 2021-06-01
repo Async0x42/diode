1b4c9fdfdf709f483383c5bb4ac68ec5d20c2a90
Async0x42 <Async0x42@users.noreply.github.com>
2021-05-31 21:29:57 -0400

    fix buttons

--- packages/client/src/components/FormButton.vue
+++ packages/client/src/components/FormButton.vue
@@ -3,6 +3,6 @@
     type="button"
     class="border border-transparent rounded-md font-medium shadow-sm text-sm text-white py-2 px-4 focus:outline-none focus:ring-2 focus:ring-offset-2"
   >
-    <slot></slot>>
+    <slot></slot>
   </button>
 </template>
--- packages/client/src/components/FormButtonCancel.vue
+++ packages/client/src/components/FormButtonCancel.vue
@@ -1,3 +1,3 @@
 <template>
-  <FormButton class="bg-white border-gray-300 text-gray-700 hover:bg-gray-50 focus:ring-indigo-500">Delete</FormButton>
+  <FormButton class="bg-white border-gray-300 text-gray-700 hover:bg-gray-50 focus:ring-indigo-500">Cancel</FormButton>
 </template>
