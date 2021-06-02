861f7f6e0fead8e4e7173aa0cc4b12770250458c
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-02 16:47:12 -0400

    fix application name in brd view

--- packages/client/src/components/BrdView.vue
+++ packages/client/src/components/BrdView.vue
@@ -25,7 +25,8 @@ const props = defineProps({
         </div>
         <div class="sm:col-span-1">
           <dt class="font-medium text-sm text-gray-500">Application</dt>
-          <dd class="mt-1 text-sm text-gray-900">{{ props.brd.application }}</dd>
+          <dd class="mt-1 text-sm text-gray-900">{{ props.brd.application?.name }}</dd>
+          <dd class="text-sm text-gray-500">{{ props.brd.application?.shortName }}</dd>
         </div>
         <div class="sm:col-span-1">
           <dt class="font-medium text-sm text-gray-500">Description</dt>
