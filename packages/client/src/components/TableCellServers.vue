5e485ff8d5e517ee7ce0676948153a835456abb4
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-01 22:10:06 -0400

    Update TableCellServers.vue

--- packages/client/src/components/TableCellServers.vue
+++ packages/client/src/components/TableCellServers.vue
@@ -11,7 +11,9 @@ const props = defineProps({
 <template>
   <td class="py-4 px-6 whitespace-nowrap">
     <router-link v-for="server in props.servers" :key="server.id" class="group" :to="{ name: 'server-view', params: { serverId: server.id } }">
-      <div class="text-sm text-gray-900 group-hover:border-l-1 group-hover:border-blue-300">{{ server?.name }}</div>
+      <div class="text-sm text-gray-900 group-hover:border-l-1 group-hover:border-blue-300" :class="{ 'mt-2': props.servers.length > 1 }">
+        {{ server?.name }}
+      </div>
       <div class="text-sm text-gray-500 group-hover:border-l-1 group-hover:border-blue-300">
         <template v-if="server?.ip">[{{ server.ip }}] </template>
         <template v-if="server?.location">[{{ server.location.shortName || server.location.name }}] </template>
