a6f10eaf105028c9330360fd675fed81ab133130
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-03 19:26:11 -0400

    wrap up vue-hooks-form in useFormActions

---
+++ packages/client/src/logic/form.ts
@@ -0,0 +1,37 @@
+import { useRouter } from 'vue-router';
+import { useAxios } from '@vueuse/integrations';
+import { useForm } from 'vue-hooks-form';
+
+export const useFormActions = <T extends { id: number }>(apiPath: string, nextRouteName: string, dataObject?: T) => {
+  const router = useRouter();
+
+  const { useField, handleSubmit } = dataObject
+    ? useForm<T>({
+        defaultValues: dataObject,
+      })
+    : useForm<T>({});
+
+  // TODO: remove async and display loading information and errors
+  const onSubmit = handleSubmit(async (formData) => {
+    if (dataObject == null) {
+      // create
+      const { data, isFinished } = await useAxios(apiPath, { method: 'POST', data: formData });
+      router.push({ name: nextRouteName });
+    } else {
+      // update
+      const { data, isFinished } = await useAxios(`${apiPath}/${dataObject.id}`, { method: 'PUT', data: formData });
+      router.push({ name: nextRouteName });
+    }
+
+    // on success, display checkmark transition and then redirect to the new/edited application
+  });
+
+  const onDelete = async () => {
+    if (dataObject != null) {
+      const { data, isFinished } = await useAxios(`${apiPath}/${dataObject.id}`, { method: 'DELETE' });
+      router.push({ name: nextRouteName });
+    }
+  };
+
+  return { onSubmit, onDelete, useField, handleSubmit };
+};
