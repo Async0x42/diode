1313992c227330a32331cf42496f93a6c933a509
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-08 23:11:30 -0400

    working

--- packages/client/src/components/Form/FormDatePicker.vue
+++ packages/client/src/components/Form/FormDatePicker.vue
@@ -2,6 +2,7 @@
 // TODO: error message example is here https://vcalendar.io/datepicker.html
 import { defineProps } from 'vue';
 import type { PropType } from 'vue';
+import { DatePicker } from 'v-calendar';
 import type { FormField } from '~/types';
 
 const props = defineProps({
@@ -15,21 +16,19 @@ const props = defineProps({
   <div>
     <label :for="props.name" class="font-medium text-sm text-gray-700 block"> {{ props.label }} </label>
     <div class="mt-1">
-      <client-only>
-        <date-picker v-model="props.field.value" :masks="{ input: 'YYYY-MM-DD' }" :update-on-input="false">
-          <template #default="{ inputValue, inputEvents }">
-            <input
-              :id="props.name"
-              :ref="props.field.ref"
-              :name="props.name"
-              :value="inputValue"
-              type="text"
-              class="rounded-md flex-grow border-gray-300 shadow-sm w-full block sm:text-sm focus:border-indigo-500 focus:ring-indigo-500"
-              v-on="inputEvents"
-            />
-          </template>
-        </date-picker>
-      </client-only>
+      <date-picker v-model="props.field.value" :masks="{ input: 'YYYY-MM-DD' }" :update-on-input="false">
+        <template #default="{ inputValue, inputEvents }">
+          <input
+            :id="props.name"
+            :ref="props.field.ref"
+            :name="props.name"
+            :value="inputValue"
+            type="text"
+            class="rounded-md flex-grow border-gray-300 shadow-sm w-full block sm:text-sm focus:border-indigo-500 focus:ring-indigo-500"
+            v-on="inputEvents"
+          />
+        </template>
+      </date-picker>
     </div>
     <slot name="note"></slot>
   </div>
--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,6 +1,8 @@
-import { ViteSSG } from 'vite-ssg';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 import generatedRoutes from 'virtual:generated-pages';
 import { setupLayouts } from 'virtual:generated-layouts';
+import { createHead } from '@vueuse/head';
 import App from './App.vue';
 import 'virtual:windi.css';
 import 'virtual:windi-devtools';
@@ -8,15 +10,19 @@ import './styles/main.css';
 
 const routes = setupLayouts(generatedRoutes);
 
-// https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(App, { routes }, async (ctx) => {
-  // install all modules under `modules/`
-  Object.values(import.meta.globEager('./modules/*.ts')).map((i) => i.install?.(ctx));
-  Object.values(import.meta.globEager('./directives/*.ts')).map((i) => i.install?.(ctx));
-
-  // fixes window undefined error in ssr, used with <client-only> component
-  if (typeof window !== 'undefined') {
-    // eslint-disable-next-line import/order
-    await import('v-calendar').then((obj) => ctx.app.component('DatePicker', obj.DatePicker));
-  }
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
+
+// https://github.com/antfu/vite-ssg
+export const app = createApp(App);
+const head = createHead();
+app.use(router);
+app.use(head);
+
+// install all modules under `modules/`
+Object.values(import.meta.globEager('./modules/*.ts')).map((i) => i.install?.({ app, router, isClient: true }));
+Object.values(import.meta.globEager('./directives/*.ts')).map((i) => i.install?.({ app, router, isClient: true }));
+
+app.mount('#app');
