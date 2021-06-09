4208462e60dc210b2df1d9c666be7b44d056d561
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-08 20:36:09 -0400

    fix window undefined error with ssr

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -9,8 +9,14 @@ import './styles/main.css';
 const routes = setupLayouts(generatedRoutes);
 
 // https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(App, { routes }, (ctx) => {
+export const createApp = ViteSSG(App, { routes }, async (ctx) => {
   // install all modules under `modules/`
   Object.values(import.meta.globEager('./modules/*.ts')).map((i) => i.install?.(ctx));
   Object.values(import.meta.globEager('./directives/*.ts')).map((i) => i.install?.(ctx));
+
+  // fixes window undefined error in ssr, used with <client-only> component
+  if (typeof window !== 'undefined') {
+    // eslint-disable-next-line import/order
+    await import('v-calendar').then((obj) => ctx.app.component('DatePicker', obj.DatePicker));
+  }
 });
