32b1111adff3c3d0fb0e426ba0d1f1260ee65da5
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-09 21:24:21 -0400

    remove ssr deps

--- packages/client/src/types.ts
+++ packages/client/src/types.ts
@@ -1,7 +1,17 @@
-import { ViteSSGContext } from 'vite-ssg';
 import type { FieldNode } from 'vue-hooks-form/dist/src/utils';
+import type { App } from 'vue';
+import type { Router, RouteRecordRaw } from 'vue-router';
+import type { HeadClient } from '@vueuse/head';
 
-export type UserModule = (ctx: ViteSSGContext) => void;
+// taken from vite-ssg
+export type UserModule<HasRouter extends boolean = true> = (ctx: {
+  app: App<Element>;
+  router: HasRouter extends true ? Router : undefined;
+  routes: HasRouter extends true ? RouteRecordRaw[] : undefined;
+  initialState: Record<string, any>;
+  head: HeadClient | undefined;
+  isClient: boolean;
+}) => void;
 
 export type FormField = {
   ref: (nodeRef: FieldNode) => void;
