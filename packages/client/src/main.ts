87f1d4c393a4592771e3948ab625533be8cb4dde
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-10 22:14:30 -0400

    working vue-auto-routing and layouts

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -3,14 +3,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router';
+import router from './plugins/auto-routes';
 
 Vue.config.productionTip = false;
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-});
 
 new Vue({
   router,
