7cc369bd12b14353a463e07821dc52b230e7b2d0
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-15 22:06:13 -0400

    update form buttons

--- packages/client/src/components/Form/FormButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <button
-    type="button"
-    class="border border-transparent rounded-md font-medium shadow-sm text-sm text-white py-2 px-4 focus:outline-none focus:ring-2 focus:ring-offset-2"
-  >
-    <slot></slot>
-  </button>
-</template>
--- packages/client/src/components/Form/FormButtonDelete.vue
+++ packages/client/src/components/Form/FormButtonDelete.vue
@@ -1,3 +1,3 @@
 <template>
-  <FormButton class="bg-red-600 hover:bg-red-700 focus:ring-red-500">Delete</FormButton>
+  <n-button type="error">Delete</n-button>
 </template>
