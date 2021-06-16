864db449b3cca9bfa82c07aef98131e461e22bd2
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-15 22:07:02 -0400

    Update ApplicationCreateEdit.vue

--- packages/client/src/components/Application/ApplicationCreateEdit.vue
+++ packages/client/src/components/Application/ApplicationCreateEdit.vue
@@ -24,37 +24,29 @@ const sslCertificates = useField('sslCertificates');
 </script>
 
 <template>
-  <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-    <form class="divide-y space-y-8 divide-gray-200 py-5 px-4 sm:px-6" @submit="onSubmit">
-      <div class="divide-y space-y-8 divide-gray-200">
-        <div>
-          <div>
-            <h3 class="font-medium text-lg text-gray-900 leading-6">Application Information</h3>
-            <p class="mt-1 text-sm text-gray-500">This information will be displayed publicly.</p>
-          </div>
+  <n-page-header class="mx-8 mt-6" title="Application Information" />
+  <n-form class="mx-12" @submit="onSubmit">
+    <div class="divide-y space-y-8 divide-gray-200">
+      <div class="mt-6 grid gap-y-6 gap-x-4 grid-cols-1 sm:grid-cols-6">
+        <n-input label="Name" :field="name" name="name" class="sm:col-span-3" />
+        <n-input label="Short Name" :field="shortName" name="shortName" class="sm:col-span-3" />
 
-          <div class="mt-6 grid gap-y-6 gap-x-4 grid-cols-1 sm:grid-cols-6">
-            <FormInput label="Name" :field="name" name="name" class="sm:col-span-3" />
-            <FormInput label="Short Name" :field="shortName" name="shortName" class="sm:col-span-3" />
-
-            <FormServerMultiSelect label="Servers" :field="servers" name="server" class="sm:col-span-3" />
-            <FormFqdnMultiSelect label="FQDNs" :field="fqdns" name="fqdns" class="sm:col-span-3" />
-            <FormBrdMultiSelect label="BRDs" :field="brds" name="brds" class="sm:col-span-3" />
-            <FormRfcMultiSelect label="RFCs" :field="rfcs" name="rfc" class="sm:col-span-3" />
-            <FormSslCertificateMultiSelect label="SSL Certificates" :field="sslCertificates" name="sslCertificates" class="sm:col-span-3" />
-            <FormTextArea label="Description" :field="description" name="description" class="sm:col-span-6" :rows="6" />
-          </div>
-        </div>
+        <FormServerMultiSelect label="Servers" :field="servers" name="server" class="sm:col-span-3" />
+        <FormFqdnMultiSelect label="FQDNs" :field="fqdns" name="fqdns" class="sm:col-span-3" />
+        <FormBrdMultiSelect label="BRDs" :field="brds" name="brds" class="sm:col-span-3" />
+        <FormRfcMultiSelect label="RFCs" :field="rfcs" name="rfc" class="sm:col-span-3" />
+        <FormSslCertificateMultiSelect label="SSL Certificates" :field="sslCertificates" name="sslCertificates" class="sm:col-span-3" />
+        <FormTextArea label="Description" :field="description" name="description" class="sm:col-span-6" :rows="6" />
       </div>
+    </div>
 
-      <div class="pt-5">
-        <div class="flex justify-end">
-          <FormButtonDelete v-if="props.application" class="mr-3 inline-flex justify-center" @click="onDelete()" />
-          <div class="flex-1"></div>
-          <FormButtonCancel @click="$router.back()" />
-          <FormButtonOk class="ml-3 inline-flex justify-center" />
-        </div>
+    <div class="pt-5">
+      <div class="flex justify-end">
+        <FormButtonDelete v-if="props.application" class="mr-3" @click="onDelete()" />
+        <div class="flex-1"></div>
+        <FormButtonCancel @click="$router.back()" />
+        <FormButtonOk class="ml-3" />
       </div>
-    </form>
-  </div>
+    </div>
+  </n-form>
 </template>
