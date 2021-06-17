a76cfac341bb936d64144530ded3e4539da9c49d
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-16 22:23:05 -0400

    fqdn work

--- packages/client/src/components/Fqdn/FqdnView.vue
+++ packages/client/src/components/Fqdn/FqdnView.vue
@@ -9,29 +9,21 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-    <div class="py-5 px-4 sm:px-6">
-      <h3 class="font-medium text-lg text-gray-900 leading-6">FQDN Information</h3>
-      <p class="mt-1 text-sm max-w-2xl text-gray-500">FQDN details and notes.</p>
-    </div>
-    <div class="border-t border-gray-200 py-5 px-4 sm:px-6">
-      <dl class="grid gap-x-4 gap-y-8 grid-cols-1 sm:grid-cols-2">
-        <div class="sm:col-span-1">
-          <dt class="font-medium text-sm text-gray-500">Name</dt>
-          <dd class="mt-1 text-sm text-gray-900">{{ props.fqdn.name }}</dd>
-        </div>
-        <div class="sm:col-span-1">
-          <dt class="font-medium text-sm text-gray-500">Server</dt>
-          <dd class="mt-1 text-sm text-gray-900">{{ props.fqdn.server?.name }}</dd>
-        </div>
-        <div class="sm:col-span-1">
-          <dt class="font-medium text-sm text-gray-500">Applications</dt>
-          <dd v-for="application in props.fqdn.applications" :key="application.id" class="mt-1 text-sm text-gray-900">
-            <template v-if="application?.shortName"> [{{ application?.shortName }}] </template>
-            {{ application.name }}
-          </dd>
-        </div>
-      </dl>
-    </div>
-  </div>
+  <n-page-header class="p-2" title="FQDN Information" />
+  <n-descriptions bordered>
+    <n-descriptions-item label="Name" :span="3">
+      <n-text tag="div" depth="1">{{ props.fqdn.name }}</n-text>
+    </n-descriptions-item>
+    <n-descriptions-item label="Server">
+      <n-text tag="div" depth="1">{{ props.fqdn.server }}</n-text>
+    </n-descriptions-item>
+    <n-descriptions-item label="Applications" :span="3">
+      <div v-for="application in props.fqdn.applications" :key="application.id" class="mb-2 group">
+        <n-text tag="div" depth="1" class="group-hover:text-teal-300">
+          <template v-if="application?.shortName"> [{{ application?.shortName }}] </template>
+          {{ application.name }}
+        </n-text>
+      </div>
+    </n-descriptions-item>
+  </n-descriptions>
 </template>
