28a8a9b8219c25f95f6fd922507c28cbe577ff66
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-16 22:14:19 -0400

    environment, network, operating system, server location, server type, and zone forms

--- packages/client/src/components/ServerType/ServerTypeView.vue
+++ packages/client/src/components/ServerType/ServerTypeView.vue
@@ -9,19 +9,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-    <div class="py-5 px-4 sm:px-6">
-      <h3 class="font-medium text-lg text-gray-900 leading-6">Server Type Information</h3>
-      <p class="mt-1 text-sm max-w-2xl text-gray-500">Server type details and notes.</p>
-    </div>
-    <div class="border-t border-gray-200 py-5 px-4 sm:px-6">
-      <dl class="grid gap-x-4 gap-y-8 grid-cols-1 sm:grid-cols-2">
-        <div class="sm:col-span-1">
-          <dt class="font-medium text-sm text-gray-500">Name</dt>
-          <dd class="mt-1 text-sm text-gray-900">{{ props.serverType.name }}</dd>
-          <dd class="mt-1 text-sm text-gray-700">{{ props.serverType.shortName }}</dd>
-        </div>
-      </dl>
-    </div>
-  </div>
+  <n-page-header class="p-2" title="Server Type Information" />
+  <n-descriptions bordered>
+    <n-descriptions-item label="Name" :span="3">
+      <n-text tag="div" depth="1">{{ props.serverType.name }}</n-text>
+      <n-text tag="div" depth="3">{{ props.serverType.shortName }}</n-text>
+    </n-descriptions-item>
+  </n-descriptions>
 </template>
