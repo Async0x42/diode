6fd5abc4d5f2bb5a35a24a426fc788e98621cd10
Async0x42 <Async0x42@users.noreply.github.com>
2021-06-22 19:43:00 -0400

    show id on work orders

--- packages/client/src/components/WorkOrder/WorkOrderListItem.vue
+++ packages/client/src/components/WorkOrder/WorkOrderListItem.vue
@@ -14,6 +14,7 @@ const props = defineProps({
     <n-td>
       <router-link class="group" :to="{ name: 'workOrder-view', params: { workOrderId: props.workOrder.id } }">
         <n-text tag="div" depth="1" class="group-hover:text-teal-300">{{ props.workOrder.name }}</n-text>
+        <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ props.workOrder.id }}</n-text>
         <n-text tag="div" depth="3" class="whitespace-nowrap group-hover:text-teal-500">
           {{ formatTableDate(props.workOrder.startDate) }} - {{ formatTableDate(props.workOrder.endDate) }}
         </n-text>
