79cd4f2cb1e770adfaf7e131140bbf4f79c01cd3
Async0x42 <Async0x42@users.noreply.github.com>
2021-07-21 18:37:08 -0400

    fix accordion bug in ticket/workorder list

--- packages/client/src/components/Ticket/TicketList.vue
+++ packages/client/src/components/Ticket/TicketList.vue
@@ -22,7 +22,7 @@ const groupedResults = computed(() => groupBy(results.value, 'status'));
 </script>
 
 <template>
-  <n-collapse default-expanded-names="Open" class="mt-3">
+  <n-collapse :default-expanded-names="['Open']" class="mt-3">
     <n-collapse-item v-for="(groupKey, index) in Object.keys(groupedResults)" :key="index" :title="groupKey" :name="groupKey">
       <TableView :headers="['Ticket', 'Status', 'Name', 'Applications', 'Servers', 'Created', 'Est Due Date', '']">
         <TicketListItem v-for="ticket in groupedResults[groupKey]" :key="ticket.id" :ticket="ticket" />
