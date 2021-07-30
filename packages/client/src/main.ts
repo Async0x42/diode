05e363db5d2292b7c7cd9f7b633203cdd4579eed
Async0x42 <Async0x42@users.noreply.github.com>
2021-07-30 14:29:29 -0400

    template/dep

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -1,3 +1,4 @@
+import 'vue-global-api';
 import { createApp } from 'vue';
 import { createHead } from '@vueuse/head';
 import App from './App.vue';
@@ -5,16 +6,12 @@ import App from './App.vue';
 // windicss layers
 import 'virtual:windi-base.css';
 import 'virtual:windi-components.css';
-// your custom styles here
-import './styles/main.css';
+
 // windicss utilities should be the last style import
 import 'virtual:windi-utilities.css';
 
 // windicss devtools support (dev only)
 import 'virtual:windi-devtools';
-
-// register vue composition api globally
-import 'vue-global-api';
 
 import 'vfonts/Lato.css';
 import 'vfonts/FiraCode.css';
