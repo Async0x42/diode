8347686e4378acf5ab5fb6ec037762ba4f2fda29
Async0x42 <Async0x42@users.noreply.github.com>
2021-08-17 12:51:48 -0400

    fix typings

--- packages/client/src/utils/arrays.ts
+++ packages/client/src/utils/arrays.ts
@@ -1,2 +1,2 @@
-export const sortByPriority = (priorities: string[], array: { [key: string]: any }[], key: string) =>
+export const sortByPriority = <T extends { [key: string]: any }>(priorities: string[], array: T[], key: string) =>
   array.sort((a, b) => priorities.indexOf(a[key]) - priorities.indexOf(b[key]));
