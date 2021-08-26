2811ae129d9c3840d7a0e2cebe8f533e130ddb4e
Async0x42 <Async0x42@users.noreply.github.com>
2021-08-25 23:36:02 -0400

    move server location from table cell to table cell tooltip

--- packages/client/src/components/Widgets/ServersWidget.vue
+++ packages/client/src/components/Widgets/ServersWidget.vue
@@ -12,6 +12,7 @@ const props = defineProps({
   <template v-if="!Array.isArray(props.servers)">
     <div class="group">
       <n-text v-if="showTitle" tag="div" depth="1" class="group-hover:text-teal-300">{{ props.servers.name }}</n-text>
+      <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ props.servers.location?.shortName || props.servers.location?.name }}</n-text>
       <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ props.servers.ip }}</n-text>
       <n-text tag="div" depth="3" class="group-hover:text-teal-500">{{ props.servers.operatingSystem?.name }}</n-text>
     </div>
@@ -19,6 +20,7 @@ const props = defineProps({
   <template v-else>
     <div v-for="server in props.servers" :key="server.id" class="mb-2 group">
       <n-text v-if="showTitle" tag="div" depth="1" class="group-hover:text-teal-300">{{ server?.name }}</n-text>
+      <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ server.location?.shortName || server.location?.name }}</n-text>
       <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ server?.ip }}</n-text>
       <n-text tag="div" depth="3" class="group-hover:text-teal-500">{{ server?.operatingSystem?.name }}</n-text>
     </div>
