c2605a59b0ac2601ff6afd8622a18ff0fc4c6ded
Aaryn Bryanton <bryanton.ac@gmail.com>
2021-10-05 14:53:48 -0400

    update workorder table

--- packages/client/src/components/WorkOrder/WorkOrderList.vue
+++ packages/client/src/components/WorkOrder/WorkOrderList.vue
@@ -2,11 +2,8 @@
 import type { IWorkOrder } from '@diode/common';
 import type { PropType } from 'vue';
 import { groupBy, reverse } from 'lodash-es';
-import type { DataTableColumn } from 'naive-ui';
 import { useRouteSearchWithData } from '~/logic';
-import { applicationsColumn, createQuickActionsColumn, detailsColumn, modifiedColumn, serversColumn, sortByPriority, workOrderColumn } from '~/utils';
-
-const router = useRouter();
+import { sortByPriority } from '~/utils';
 
 const props = defineProps({
   workOrders: { type: Array as PropType<IWorkOrder[]>, required: true },
@@ -25,20 +22,39 @@ const { results } = useRouteSearchWithData(props.workOrders, [
 const sortPriorities = reverse(['New', 'In progress', 'Waiting on others']);
 const sortedResults = computed(() => reverse(sortByPriority(sortPriorities, results.value, 'status')));
 const groupedResults = computed(() => groupBy(sortedResults.value, 'status'));
-const columns: DataTableColumn[] = [
-  workOrderColumn,
-  detailsColumn,
-  applicationsColumn,
-  serversColumn,
-  { ...modifiedColumn, defaultSortOrder: 'descend' } as DataTableColumn,
-  createQuickActionsColumn((row: any) => router.push({ name: 'workOrder-edit', params: { workOrderId: row.id } })),
-];
 </script>
 
 <template>
   <n-collapse :default-expanded-names="['New', 'In progress', 'Waiting on others']" class="mt-3">
     <n-collapse-item v-for="(groupKey, index) in Object.keys(groupedResults)" :key="index" :title="groupKey" :name="groupKey">
-      <n-data-table :columns="columns" :data="groupedResults[groupKey]" size="small" />
+      <DataTable :value="groupedResults[groupKey]" responsive-layout="scroll">
+        <Column field="workOrder" header="Work Order">
+          <template #body="slotProps">
+            <TableCellWorkOrder :work-order="slotProps.data" />
+          </template>
+        </Column>
+        <Column field="details" header="Details"></Column>
+        <Column field="applications" header="Applications">
+          <template #body="slotProps">
+            <TableCellApplications :applications="slotProps.data.applications" :use-short-name="true" />
+          </template>
+        </Column>
+        <Column field="servers" header="Servers">
+          <template #body="slotProps">
+            <TableCellServers :servers="slotProps.data.servers" />
+          </template>
+        </Column>
+        <Column field="modifiedOn" header="Updated">
+          <template #body="slotProps">
+            <TableCellModified :date="slotProps.data.modifiedOn" />
+          </template>
+        </Column>
+        <Column>
+          <template #body="slotProps">
+            <TableCellQuickActions @edit="$router.push({ name: 'workOrder-edit', params: { workOrderId: slotProps.data.id } })" />
+          </template>
+        </Column>
+      </DataTable>
     </n-collapse-item>
   </n-collapse>
 </template>
