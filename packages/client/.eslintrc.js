9b2a1c7bd2697d9314754c16eb824e6077bed3bd
Async0x42 <Async0x42@users.noreply.github.com>
2021-10-26 17:39:15 -0400

    fix some ts errors

--- packages/client/.eslintrc.js
+++ packages/client/.eslintrc.js
@@ -2,6 +2,5 @@ module.exports = {
   extends: ['@antfu', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
   rules: {
     'no-console': 'off',
-    '@typescript-eslint/no-unused-vars': 'off', // doesn't work well with script setup -> ts
   },
 };
