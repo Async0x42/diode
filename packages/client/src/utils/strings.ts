b76c46c371e53e568eed372073de8ecb6e50dc03
Async0x42 <Async0x42@users.noreply.github.com>
2022-01-25 17:20:42 -0500

    chore: add source of snippet

--- packages/client/src/utils/strings.ts
+++ packages/client/src/utils/strings.ts
@@ -1,1 +1,2 @@
+// https://stackoverflow.com/questions/5999792/new-line-in-textarea-to-be-converted-to-br
 export const replaceLinebreakWithHtml = (text?: string) => text?.replace(/\r\n|\r|\n/g, "<br />")
