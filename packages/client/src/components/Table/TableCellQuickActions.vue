ed7319d148ddf828dc968d9917a4d35b813160b5
Async0x42 <Async0x42@users.noreply.github.com>
2022-01-25 14:55:40 -0500

    ux: improve quick actions cell layout

--- packages/client/src/components/Table/TableCellQuickActions.vue
+++ packages/client/src/components/Table/TableCellQuickActions.vue
@@ -13,26 +13,31 @@ const onBrowse = () => {
 </script>
 
 <template>
-  <n-td class="text-right max-w-31.5">
-    <TableButton v-if="props.browse" @click="onBrowse">
-      <template #icon>
-        <heroicons-solid-eye />
-      </template>
-    </TableButton>
-    <TableButton v-if="props.email" @click="$emit('email')">
-      <template #icon>
-        <heroicons-solid-mail />
-      </template>
-    </TableButton>
-    <TableButton v-if="props.phone" @click="$emit('phone')">
-      <template #icon>
-        <heroicons-solid-phone />
-      </template>
-    </TableButton>
-    <TableButton @click="$emit('edit')">
-      <template #icon>
-        <heroicons-solid-pencil />
-      </template>
-    </TableButton>
+  <n-td
+    class="text-right"
+    :class="[(props.email || props.phone || props.browse) ? 'min-w-29 max-w-29' : 'min-w-13 max-w-13']"
+  >
+    <div class="flex flex-wrap">
+      <TableButton v-if="props.browse" @click="onBrowse">
+        <template #icon>
+          <heroicons-solid-eye />
+        </template>
+      </TableButton>
+      <TableButton v-if="props.email" @click="$emit('email')">
+        <template #icon>
+          <heroicons-solid-mail />
+        </template>
+      </TableButton>
+      <TableButton v-if="props.phone" @click="$emit('phone')">
+        <template #icon>
+          <heroicons-solid-phone />
+        </template>
+      </TableButton>
+      <TableButton @click="$emit('edit')">
+        <template #icon>
+          <heroicons-solid-pencil />
+        </template>
+      </TableButton>
+    </div>
   </n-td>
 </template>
