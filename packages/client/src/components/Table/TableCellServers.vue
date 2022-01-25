d24858e8eb94fa9d79ea7ae8b29aae8916f98717
Async0x42 <Async0x42@users.noreply.github.com>
2022-01-25 13:20:36 -0500

    ux: improve servers cell and widget

--- packages/client/src/components/Table/TableCellServers.vue
+++ packages/client/src/components/Table/TableCellServers.vue
@@ -10,20 +10,18 @@ const props = defineProps({
 <template>
   <n-td class="whitespace-nowrap">
     <router-link
-      v-for="server in props.servers"
+      v-for="(server, index) in props.servers"
       :key="server.id"
-      :class="{ 'mb-2': props.servers.length > 1 }"
       class="group"
       :to="{ name: 'server-view', params: { serverId: server.id } }"
     >
       <ServerTooltip :server="server">
-        <div>
-          <n-text tag="div" depth="1" class="group-hover:text-teal-300">{{ server?.name }}</n-text>
-          <n-text tag="div" depth="3" class="group-hover:text-teal-500">
-            <template v-if="server.environment">[{{ server.environment.shortName || server.environment.name }}] </template>
-            <template v-if="server.types.length > 0">
-              <template v-for="serverType in server.types" :key="serverType.id"> [{{ serverType.shortName || serverType.name }}] </template>
-            </template>
+        <div :class="{ 'mb-2': props.servers.length > 1 && index !== props.servers.length - 1 }">
+          <n-text tag="div" depth="1"><n-tag size="small" class="cursor-pointer whitespace-nowrap group-hover:text-teal-300">{{ server?.name }}</n-tag></n-text>
+          <n-text tag="div" depth="3">
+            <n-tag size="small" class="cursor-pointer  whitespace-nowrap group-hover:text-teal-300" v-if="server.location">{{ server.location?.shortName || server.location?.name }}</n-tag>
+            <n-tag size="small" class="cursor-pointer  whitespace-nowrap group-hover:text-teal-300" v-if="server.environment">{{ server.environment?.shortName || server.environment?.name }}</n-tag>
+            <n-tag v-for="type in server.types" size="small" class="cursor-pointer  whitespace-nowrap group-hover:text-teal-300">{{ type?.shortName || type?.name }}</n-tag>
           </n-text>
         </div>
       </ServerTooltip>
--- packages/client/src/components/Widgets/ServersWidget.vue
+++ packages/client/src/components/Widgets/ServersWidget.vue
@@ -11,8 +11,12 @@ const props = defineProps({
 <template>
   <template v-if="!Array.isArray(props.servers)">
     <div class="group">
-      <n-text v-if="showTitle" tag="div" depth="1" class="group-hover:text-teal-300">{{ props.servers.name }}</n-text>
-      <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ props.servers.location?.shortName || props.servers.location?.name }}</n-text>
+      <n-text v-if="showTitle" tag="div" depth="1" class="group-hover:text-teal-300">{{ props.servers.name }} TEST</n-text>
+      <div>
+        <n-tag size="small" class="whitespace-nowrap group-hover:text-teal-400" v-if="props.servers.location">{{ props.servers.location?.shortName || props.servers.location?.name }}</n-tag>
+        <n-tag size="small" class="whitespace-nowrap group-hover:text-teal-400" v-if="props.servers.environment">{{ props.servers.environment?.shortName || props.servers.environment?.name }}</n-tag>
+      </div>
+      <n-tag v-for="type in props.servers.types" size="small" class="whitespace-nowrap group-hover:text-teal-400">{{ type?.shortName || type?.name }}</n-tag>
       <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ props.servers.ip }}</n-text>
       <n-text tag="div" depth="3" class="group-hover:text-teal-500">{{ props.servers.operatingSystem?.name }}</n-text>
     </div>
@@ -20,7 +24,11 @@ const props = defineProps({
   <template v-else>
     <div v-for="server in props.servers" :key="server.id" class="mb-2 group">
       <n-text v-if="showTitle" tag="div" depth="1" class="group-hover:text-teal-300">{{ server?.name }}</n-text>
-      <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ server.location?.shortName || server.location?.name }}</n-text>
+      <div>
+        <n-tag size="small" class="whitespace-nowrap group-hover:text-teal-400" v-if="server.location">{{ server.location?.shortName || server.location?.name }}</n-tag>
+        <n-tag size="small" class="whitespace-nowrap group-hover:text-teal-400" v-if="server.environment">{{ server.environment?.shortName || server.environment?.name }}</n-tag>
+      </div>
+      <n-tag v-for="type in server.types" size="small" class="whitespace-nowrap group-hover:text-teal-400">{{ type?.shortName || type?.name }}</n-tag>
       <n-text tag="div" depth="2" class="group-hover:text-teal-400">{{ server?.ip }}</n-text>
       <n-text tag="div" depth="3" class="group-hover:text-teal-500">{{ server?.operatingSystem?.name }}</n-text>
     </div>
